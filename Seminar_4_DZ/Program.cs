171d1b325b578f6287349c0f92e8a779a9153b8b
Kayabo <griha3565478@gmail.com>
2022-11-28 11:31:18 +0300

    решил первые две задачки

---
+++ Seminar_4_DZ/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача № 25 напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+
+// Console.WriteLine("Введите число А которое возведем в степень числа B :");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.WriteLine();
+// Console.WriteLine("Введите число B :");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.WriteLine();
+// Console.WriteLine($"А в Степени В будет равно {PowAInB(a, b)}");
+
+// int PowAInB(int one, int two)
+// {   
+//     int c = one;
+//     for(int i = 1; i < two; i++)
+//     {
+//         one = one * c;
+//     }
+//     return one;
+// }
+
+
+
+
+// // Здача №27.
+
+// Console.WriteLine("Введите число, посчитаем сумму его цифр :");
+// string number = Console.ReadLine()!;
+// Console.WriteLine();
+// Console.WriteLine($"Сумма чисел числа {number}, равна {SumNum(number)}");
+
+// int SumNum(string num)
+// {
+//     int len =  number.Length;
+//     int result = 0;
+//     int a = 0;
+    
+//     for (int i = 0; i < len; i ++)
+//     {
+//         a = Convert.ToInt32(Convert.ToString(num[i]));
+//         result += a;
+//     }
+//     return result;
+// }
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
