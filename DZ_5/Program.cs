f64fb716d1021bd9631afa7debbd340c8e3df48c
Kayabo <griha3565478@gmail.com>
2022-11-30 11:03:14 +0300

    решил 37 задачу, на костылях

--- DZ_5/Program.cs
+++ DZ_5/Program.cs
@@ -1,72 +1,108 @@
-﻿// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+﻿// // Задача 34
 
-// Console.WriteLine($"Массив:  [{String.Join(" , ",NewList())}]");
+// int[]  array = GetArray(4, 100, 999);
+// Console.WriteLine($"[{String.Join(",", array)}]");
+// int counter = 0;
 
-// int[] NewList()
+// foreach(int el in array)
+// {
+//     if(el % 2 ==0)
+//     {
+//         counter += 1;
+//     }
+// }
+// Console.WriteLine($"Колличество четных элементов массива равно {counter}");
+
+// // --------------------Методы-------------------------
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
+
+
+
+// // Задача 36
+// int[] mass = NewList();
+// Console.WriteLine($"Сумма Элементов с нечетным индексом, массива [{String.Join(" , ",mass)}], равна {GetSum(mass)}");
+
+// int GetSum(int[] array)
+// {
+//     int summ = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(i % 2 != 0)
+//         {
+//             summ += array[i];
+//         }
+//     }
+//     return summ;   
+// }
+
+
+// int[]  NewList()
 // {
 //     int[] list = new int[4];
-//     int len  = list.Length;
-//     for (int i = 0; i < len; i ++)
+//     for (int i = 0; i < list.Length; i ++)
 //     {
-//         list[i] = new Random().Next(100,999);   
+//         list[i] = new Random().Next(1,999);
+
 //     }
 //     return list;
 // }
 
 
 
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// [345, 897, 568, 234] -> 2
+// Задача 37
+int[] array = NewList();
 
-//// Первый вариант.
-// Console.WriteLine($"Количество четных чисел в списке {NewList()}");
+Console.WriteLine($" произведение пар чисел в одномерном массиве [{String.Join(" , ", array)}], будет  [{String.Join(" , ", PairedNumbers(array))}]");
 
-// int NewList()
-// {
-//     int[] list = new int[4];
-//     int len  = list.Length;
-//     int count = 0;
-//     for (int i = 0; i < len; i ++)
-//     {
-//         list[i] = new Random().Next(100,999);
-//         if(list[i] % 2 == 00)
-//         {
-//             count += 1;
-//         }
-//     }
-//     return count;
-// }
+// ----------------------------------Методы-------------
 
-//// Второй Вариант.
+int[]  NewList()
+{   
+    int LenList = new Random().Next(3,9);
+    int[] list = new int[LenList];
+    for (int i = 0; i < list.Length; i++)
+    {
+        list[i] = new Random().Next(1,9);
 
-// int[] two_list  = {122, 542, 346, 445};
-// Console.WriteLine($"Количество четных чисел в списке {Counting_result(two_list)}");
+    }
+    return list;
+}
 
-// // int[] test_list = NewList();
+int[] PairedNumbers(int[] mass)
+{   
+    int len = 0;
+    if(mass.Length % 2 == 0)
+    {
+        len = mass.Length / 2;
+    }
+    else
+    {
+        len = mass.Length / 2 + 1;
+    }
+    int[] newMass = new int[len];
+    int indexA = 0;
+    int indexB = mass.Length - 1; 
+    for (int i = 0; i < len; i++)
+    {
+        newMass[i] = mass[indexA] * mass[indexB];
+        indexA += 1;
+        indexB -= 1;
+    }
+    if(mass.Length % 2 != 0)
+    {
+        newMass[newMass.Length / 2 + 1] = Convert.ToInt32(Math.Sqrt(newMass[newMass.Length / 2 + 1]));
+    }
 
-// int Counting_result(int[] list)
-// {
-//     int len = list.Length;
-//     int counter = 0;
-//     for(int i = 0; i < len; i++)
-//     {
-//         if(list[i] % 2 == 0)
-//         {
-//             counter += 1;
-//         }
-//     }
-//     return counter;
-// }
- 
-//  int[] NewList()
-// {
-//     int[] list = new int[4];
-//     int len  = list.Length;
-//     for (int i = 0; i < len; i ++)
-//     {
-//         list[i] = new Random().Next(100,999);   
-//     }
-//     return list;
-// }
+    return newMass;
+}
 
