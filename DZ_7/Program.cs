50d0c1a8fe84a9670026d00b5ea3b2d6bc745e84
Kayabo <griha3565478@gmail.com>
2022-12-06 15:23:36 +0300

    Решение вешает терминал, разораться с методом поиска

--- DZ_7/Program.cs
+++ DZ_7/Program.cs
@@ -36,3 +36,60 @@
 //         Console.WriteLine("");
 //     }
 // }
+
+// Задача 50:
+int a = new Random().Next(1, 7);
+int b = new Random().Next(1, 7);
+int[,] list = NewList(a, b);
+PrintArray(list);
+SearchByIndex(list, a, b);
+
+
+//---------------------------Методы -----------------------
+
+int[,]  NewList(int line, int vertical)      // Создает двумерный массив, заполненный целыми числами, с указанным кол-вом строк и линий
+{   
+    int[,] list = new int[line, vertical];
+    for (int i = 0; i < line; i++)
+    {   
+        for(int j = 0; j < vertical; j ++)
+        {
+            list[i, j] = new Random().Next(0,9);
+        }
+    }
+    return list;
+}
+
+
+void PrintArray(int[,] array)               // Выводит в консоль  массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+void SearchByIndex(int[,] array, int line, int vertical)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   
+        int counter = 0;
+        for (int j = 0; j < array.GetLength(1); i++)
+        {
+            if(i == line && j == vertical)
+            {
+                Console.WriteLine(array[i, j]);
+                break;
+            }
+        
+        }
+    }
+    
+}
