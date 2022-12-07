ee275f19f766d2101c92ea550a48e2137e4b98ad
Kayabo <griha3565478@gmail.com>
2022-12-07 10:53:46 +0300

    50 задача уничтожена)

--- DZ_7/Program.cs
+++ DZ_7/Program.cs
@@ -38,58 +38,62 @@
 // }
 
 // Задача 50:
-int a = new Random().Next(1, 7);
-int b = new Random().Next(1, 7);
-int[,] list = NewList(a, b);
-PrintArray(list);
-SearchByIndex(list, a, b);
+// int a1 = new Random().Next(1,10);
+// int b1 = new Random().Next(1,10);
+// int[,] list = NewList(a1, b1);
+// PrintArray(list);
+// int a = new Random().Next(1,10);
+// int b = new Random().Next(1,10);
+// Console.WriteLine($"Число с индексом {a} и {b}");
+// SearchByIndex(list, a, b);
 
 
-//---------------------------Методы -----------------------
+// //---------------------------Методы -----------------------
 
-int[,]  NewList(int line, int vertical)      // Создает двумерный массив, заполненный целыми числами, с указанным кол-вом строк и линий
-{   
-    int[,] list = new int[line, vertical];
-    for (int i = 0; i < line; i++)
-    {   
-        for(int j = 0; j < vertical; j ++)
-        {
-            list[i, j] = new Random().Next(0,9);
-        }
-    }
-    return list;
-}
+// int[,]  NewList(int line, int vertical)      // Создает двумерный массив, заполненный целыми числами, с указанным кол-вом строк и линий
+// {   
+//     int[,] list = new int[line, vertical];
+//     for (int i = 0; i < line; i++)
+//     {   
+//         for(int j = 0; j < vertical; j ++)
+//         {
+//             list[i, j] = new Random().Next(0,9);
+//         }
+//     }
+//     return list;
+// }
+
+// void PrintArray(int[,] array)               // Выводит в консоль  массив
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+// void SearchByIndex(int[,] array, int line, int vertical)
+// {
+//     int counter = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (i == line && j == vertical)
+//             {
+//                 counter += 1;
+//             }
+//         }
+//     }
+//     if (counter > 0){Console.WriteLine(list[line, vertical]);}
+//     else{Console.WriteLine("такого числа нет");}
+// }
 
 
-void PrintArray(int[,] array)               // Выводит в консоль  массив
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
-    }
-}
 
 
-void SearchByIndex(int[,] array, int line, int vertical)
-{   
-    bool h = false;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); i++)
-        {
-            if(i == line && j == vertical)
-            {
-                h = true;
-            }
-        }
-        if(h ==  true){Console.WriteLine(array[line, vertical]);}
-        else{Console.WriteLine("Такой цифры нет");}
-    }
-    
-}
